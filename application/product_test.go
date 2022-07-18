1d106586853bdea262cb1e941246d593dc336927
Thales Nascimento <thales.silva.nascimento@gmail.com>
2022-07-18 15:50:46 -0300

    test: ensure product service disable a product

---
+++ application/product_test.go
@@ -0,0 +1,45 @@
+package application_test
+
+import (
+	"testing"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+	"github.com/thalessn/go-hexagonal/application"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+
+	product.Price = 0
+	err = product.Enable()
+	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.ENABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "the price must be zero in order to have the product disabled", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/thalessn/go-hexagonal
+
+go 1.16
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/satori/go.uuid v1.2.0
+	github.com/spf13/cobra v1.5.0 // indirect
+	github.com/stretchr/testify v1.8.0
+)
